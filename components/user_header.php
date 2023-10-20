e4b972957a146255fc863cbd2ecbe93aaa40fef4
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-10-20 13:41:18 -0300

    fix: home.php problema caché | modified: style.css

--- components/user_header.php
+++ components/user_header.php
@@ -11,24 +11,51 @@ if(isset($message)){
 }
 ?>
 
-<header>
-    <a href="#" class="logo"><img src="images/burger_logo.png" alt="Burger Logo" height="22">FastFood</a>
+<header class="header">
+    <section class="flex">
+        <a href="#" class="logo"><img src="images/burger_logo.png" alt="Burger Logo" height="22">FastFood</a>
 
-    <div id="menu-bar" class="fas fa-bars"></div>
+        <div id="menu-bar" class="fas fa-bars"></div>
 
-    <nav class="navbar">
-        <?php
-            $count_user_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-            $count_user_cart_items -> execute([$user_id]);
-            $total_user_cart_items = $count_user_cart_items -> rowCount();
-        ?>
-        <a href="home.php">home</a>
-        <a href="about.php">about</a>            
-        <a href="menu.php">menu</a>
-        <a href="order.php">orders</a>
-        <a href="contact.php">contact</a>
-        <a href="search.php"><i class="fas fa-search"></i></a>
-        <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_user_cart_items; ?>)</span></a>
-        <a href="user-btn"><i class="fas fa-user"></i></a>
-    </nav>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>            
+            <a href="menu.php">menu</a>
+            <a href="order.php">orders</a>
+            <a href="contact.php">contact</a>
+        </nav>
+
+        <div class="icons">
+            <?php
+                $count_user_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                $count_user_cart_items -> execute([$user_id]);
+                $total_user_cart_items = $count_user_cart_items -> rowCount();
+            ?>
+            <a href="search.php"><i class="fas fa-search"></i></a>
+            <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_user_cart_items; ?>)</span></a>
+            <a href="user-btn"><i class="fas fa-user"></i></a>
+        </div>
+
+        <div class="profile">
+            <?php
+                $select_profile = $conn -> prepare("SELECT * FROM `users` WHERE id = ?");
+                $select_profile -> execute([$user_id]);
+                if($select_profile -> rowCount() > 0){
+                    $fetch_profile = $select_profile -> fetch(PDO::FETCH_ASSOC);
+            ?>
+            <p class="name"><?= $fetch_profile['name']; ?></p>
+            <div class="flex">
+                <a href="profile.php" class="btn">perfil</a>
+                <a href="components/user_logout.php" onclick="return confirm('¿quieres cerrar sesión?');" class="delete-btn">cerrar sesión</a> 
+            </div>
+            <?php
+                }else{
+            ?>
+            <p class="name">por favor, inicia sesión</p>
+            <a href="login.php" class="btn">login</a>
+            <?php
+                }        
+            ?>
+        </div>
+    </section>    
 </header>
--- home.php
+++ home.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['user_id'])){
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" >
 
 </head>
 <body>
